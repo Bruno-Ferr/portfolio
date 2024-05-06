3c38414568e8d558769b15634699eeee1a0b4cd7
Bruno Ferreira <fbruno233@gmail.com>
2024-05-06 16:51:47 -0300

    Portfolio page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,217 @@
+"use client"
 import Image from "next/image";
+import Link from "next/link";
+import { AnimatePresence, motion, useAnimate, useInView, usePresence, useScroll, useTransform } from "framer-motion"
+import { useEffect, useRef } from "react";
+import { AnimatedText } from "@/components/animations/typpingText";
+import img1 from '@/../public/icon _next js_.png'
+import img2 from '@/../public/icon _nodejs_.svg'
+import img3 from '@/../public/icon _react_.svg'
+import img4 from '@/../public/icon _solidity_.svg'
+import img5 from '@/../public/icon_typescript.png'
+import img6 from '@/../public/icon _MySQL_.svg'
+import img7 from '@/../public/icon _tailwindcss_.svg'
+import img8 from '@/../public/icon _ethereum_.svg'
 
 export default function Home() {
+  
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className="background w-full h-screen bg-no-repeat bg-cover flex items-center">
+        <div className="max-w-screen-xl m-auto">
+          <div className="h-fit w-fit py-8 px-36 bg-gray-500 rounded-md bg-clip-padding backdrop-filter backdrop-blur-2xl bg-opacity-5"> 
+            <motion.div
+               initial={{x: -20, opacity: 0}}
+               viewport={{ once: true }}
+               animate={{x: 0, opacity: 1}}
+               transition={{duration: .75, delay: 0.2}}
+            >
+              <div className="bg-gradient-to-tr from-red-700 to-fuchsia-700 space-y w-fit p-1 rounded-full mb-8">
+                <div className="bg-indigo-950 backdrop-filter backdrop-blur-2xl w-fit p-1 rounded-full">
+                    <Image height={72} width={72} className="rounded-full" src="https://www.github.com/bruno-ferr.png" alt="Perfil" /> 
+                </div>
+              </div>
+            </motion.div>
+            <AnimatedText once text={`Hi, I'm Bruno Ferreira Web3 fullstack developer`} el="h2" className="text-6xl font-semibold text-white font-spaceGrotesk" />
+            <motion.div
+               initial={{y: 20, opacity: 0}}
+               viewport={{ once: true }}
+               animate={{y: 0, opacity: 1}}
+               transition={{duration: .75, delay: 0.2}}
+            >
+              <div className="flex mt-6 gap-3 text-gray-300">
+                <p>São Paulo, Brasil</p>
+                <div className="w-[1px] bg-white rounded-lg"></div>
+                <p>+55 (11) 94298-3604</p>
+                <div className="w-[1px] bg-white rounded-lg"></div>
+                <p>fbruno233@gmail.com</p>
+              </div>
+            </motion.div>
+          </div>
         </div>
+        <div className="w-1/3"></div> 
       </div>
+      <div className="relative w-full bg-neutral-900 bg-no-repeat bg-cover text-white">
+        <div className="max-w-screen-xl m-auto">
+          <div className="relative w-fit">
+            <motion.div 
+              viewport={{ once: true }}
+              animate={{x: [-100, 0], opacity: [0, 1], offset: ["end end", "end end"]}}
+              transition={{duration: 0.5, delay:0.25}}
+            >
+              <h2 className="font-bold text-[140px] uppercase text-neutral-800 leading-none">Skills</h2>
+              <p className="absolute bottom-0 right-0 mb-6 mr-4 text-xl font-medium text-neutral-200">My focused technologies</p>
+            </motion.div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          </div>
+          <div className="flex p-28 justify-center">
+            <div className="grid grid-cols-5 gap-6 items-center max-w-screen-lg m-auto">
+              <div className="relative">
+                <div className="border rounded-lg w-36 h-28 flex items-center justify-center text-transparent hover:bg-white hover:text-black relative">
+                  <Image height={72} width={72} src={img1} alt="next.js icon" />
+                  <p className="text-lg font-semibold absolute uppercase">Next.js</p>
+                </div>
+                <motion.div
+                  variants={{
+                    hidden: { left: 0 },
+                    visible: { left: "-100%"}
+                  }}
+                  initial={{left: 0, width: "100%"}}
+                  animate={{width: "0%", transitionEnd: {display: "none", opacity: 0}}}
+                  transition={{
+                    duration: 0.5, ease: 'easeIn'
+                  }}
+                  
+                  className="absolute top-0 bg-purple-700 w-36 h-28"
+                />
+              </div>
+              <div className="border rounded-lg w-36 h-28 flex items-center justify-center text-transparent hover:bg-white hover:text-black relative">
+                <Image height={72} width={72} src={img2} alt="next.js icon" />
+                <p className="text-lg font-semibold absolute uppercase">Node.js</p>
+              </div>
+              <div className="border rounded-lg w-36 h-28 flex items-center justify-center text-transparent hover:bg-white hover:text-black relative">
+                <Image height={72} width={72} src={img3} alt="next.js icon" />
+                <p className="text-lg font-semibold absolute uppercase">React</p>
+              </div>
+              <div className="border rounded-lg w-36 h-28 flex items-center justify-center text-transparent hover:bg-white hover:text-black relative">
+                <Image height={52} width={52} src={img4} alt="next.js icon" />
+                <p className="text-lg font-semibold absolute uppercase">Solidity</p>
+              </div>
+              <div className="border rounded-lg w-36 h-28 flex items-center justify-center text-transparent hover:bg-white hover:text-black relative">
+                <Image height={72} width={72} src={img5} alt="next.js icon" />
+                <p className="text-lg font-semibold absolute uppercase">Typescript</p>
+              </div>
+              <div></div>
+              <div className="border rounded-lg w-36 h-28 flex items-center justify-center text-transparent hover:bg-white hover:text-black relative">
+                <Image height={72} width={72} src={img6} alt="next.js icon" />
+                <p className="text-lg font-semibold absolute uppercase">MySQL</p>
+              </div>
+              <div className="border rounded-lg w-36 h-28 flex items-center justify-center text-transparent hover:bg-white hover:text-black relative">
+                <Image height={72} width={72} src={img7} alt="next.js icon" />
+                <p className="text-lg font-semibold absolute uppercase">Tailwind</p>
+              </div>
+              <div className="border rounded-lg w-36 h-28 flex items-center justify-center text-transparent hover:bg-white hover:text-black relative">
+                <Image height={52} width={52} src={img8} alt="next.js icon" />
+                <p className="text-lg font-semibold absolute uppercase">Ethereum</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> 
+      <div className="relative w-full h-screen bg-black bg-no-repeat bg-cover text-center text-white">
+        <div className="max-w-screen-xl m-auto">
+          <div className="relative w-fit">
+          <motion.div 
+            viewport={{ once: true }}
+            whileInView={{x: [-100, 0], opacity: [0, 1], offset: ["end end", "end end"]}}
+            transition={{duration: 0.5, delay:0.25}}
+          >
+            <h2 className="font-bold text-[140px] uppercase text-neutral-800 leading-none font-inter">Projects</h2>
+            <p className="absolute bottom-0 right-0 mb-6 mr-4 text-xl font-medium text-neutral-200">Take a look at my projects</p>
+          </motion.div>
+          </div>
+          <div className="flex items-center justify-center mt-12 gap-3">
+            <div className="p-4 border max-w-sm">
+              <Image height={16} width={16} className="w-72 h-56 " src="" alt="" />
+              <div className="text-start mt-4">
+                <h3 className="text-lg font-semibold">AposentaEu</h3>
+                <p>A project to calculate social previdence. Create from scratch, integrated with payment system.</p>
+              </div>
+              <div className="flex mt-4 gap-2">
+                <p className="py-[0.5px] px-2 bg-purple-400 bg-opacity-40 rounded-xl">Next.js</p>
+                <p className="py-[0.5px] px-2 bg-purple-400 bg-opacity-40 rounded-xl">Node.js</p>
+                <p className="py-[0.5px] px-2 bg-purple-400 bg-opacity-40 rounded-xl">Postgress</p>
+              </div>
+              <div className="mt-8">
+                <button className="rounded-sm border border-gray-500 bg-gradient-to-br from-blue-300 to-violet-600 px-10 py-2">Visit Site</button>
+                <Link href='/' className="px-10 py-2">Code</Link>
+              </div>
+            </div>
+            <div className="p-4 border max-w-sm">
+              <Image height={16} width={16} className="w-72 h-56 " src="" alt="" />
+              <div className="text-start mt-4">
+                <h3 className="text-lg font-semibold">AposentaEu</h3>
+                <p>A project to calculate social previdence. Create from scratch, integrated with payment system.</p>
+              </div>
+              <div className="flex mt-4 gap-2">
+                <p className="py-[0.5px] px-2 bg-purple-400 bg-opacity-40 rounded-xl">Next.js</p>
+                <p className="py-[0.5px] px-2 bg-purple-400 bg-opacity-40 rounded-xl">Node.js</p>
+                <p className="py-[0.5px] px-2 bg-purple-400 bg-opacity-40 rounded-xl">Postgress</p>
+              </div>
+              <div className="mt-8">
+                <button className="rounded-sm border border-gray-500 bg-gradient-to-br from-blue-300 to-violet-600 px-10 py-2">Visit Site</button>
+                <Link href={'/'} className="px-10 py-2">Code</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> 
     </main>
   );
 }
+
+
+{/* <div className="relative w-full h-screen bg-black bg-no-repeat bg-cover text-center text-white">
+        <div className="max-w-screen-xl m-auto">
+          <div className="relative w-fit">
+            <h2 className="font-bold text-[140px] uppercase text-neutral-800 leading-none">Projects</h2>
+            <p className="absolute bottom-0 right-0 mb-6 mr-4 text-xl font-medium text-neutral-200">Take a look at my projects</p>
+          </div>
+          <div className="flex items-center justify-center mt-12 gap-3">
+            <div className="p-4 border max-w-sm">
+              <Image height={16} width={16} className="w-72 h-56 " src="./public/user.jpg" alt="">
+              <div className="text-start mt-4">
+                <h3 className="text-lg font-semibold">AposentaEu</h3>
+                <p>A project to calculate social previdence. Create from scratch, integrated with payment system.</p>
+              </div>
+              <div className="flex mt-4 gap-2">
+                <p className="py-[0.5px] px-2 bg-purple-400 bg-opacity-40 rounded-xl">Next.js</p>
+                <p className="py-[0.5px] px-2 bg-purple-400 bg-opacity-40 rounded-xl">Node.js</p>
+                <p className="py-[0.5px] px-2 bg-purple-400 bg-opacity-40 rounded-xl">Postgress</p>
+              </div>
+              <div className="mt-8">
+                <button className="rounded-sm border border-gray-500 bg-gradient-to-br from-blue-300 to-violet-600 px-10 py-2">Visit Site</button>
+                <a className="px-10 py-2">Code</a>
+              </div>
+            </div>
+            <div className="p-4 border max-w-sm">
+              <Image height={16} width={16} className="w-72 h-56 " src="./public/user.jpg" alt="">
+              <div className="text-start mt-4">
+                <h3 className="text-lg font-semibold">AposentaEu</h3>
+                <p>A project to calculate social previdence. Create from scratch, integrated with payment system.</p>
+              </div>
+              <div className="flex mt-4 gap-2">
+                <p className="py-[0.5px] px-2 bg-purple-400 bg-opacity-40 rounded-xl">Next.js</p>
+                <p className="py-[0.5px] px-2 bg-purple-400 bg-opacity-40 rounded-xl">Node.js</p>
+                <p className="py-[0.5px] px-2 bg-purple-400 bg-opacity-40 rounded-xl">Postgress</p>
+              </div>
+              <div className="mt-8">
+                <button className="rounded-sm border border-gray-500 bg-gradient-to-br from-blue-300 to-violet-600 px-10 py-2">Visit Site</button>
+                <a className="px-10 py-2">Code</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> */}
